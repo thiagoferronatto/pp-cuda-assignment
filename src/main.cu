3910f29b787174c254cedcc4edf4d57775791f40
Thiago Ferronatto <thiagoferronatto18@gmail.com>
2022-10-19 00:30:07 -0400

    Variable name changed. LWB.

--- src/main.cu
+++ src/main.cu
@@ -26,27 +26,27 @@ int main(int argc, char** argv, char** envp) {
   if (argc != 2) exit(fputs("Syntax: ./program <input file path>\n", stderr));
 
   // Trying to open input file, exiting on failure
-  FILE* input_file_header = fopen(argv[1], "r");
-  if (!input_file_header) exit(fputs("Input file not found\n", stderr));
+  FILE* input_file_handle = fopen(argv[1], "r");
+  if (!input_file_handle) exit(fputs("Input file not found\n", stderr));
 
   // Reading lengths of both DNA sequences
   ca::types::U16 s_length, r_length;
-  fscanf(input_file_header, "%hu %hu", &s_length, &r_length);
-  fgetc(input_file_header);
+  fscanf(input_file_handle, "%hu %hu", &s_length, &r_length);
+  fgetc(input_file_handle);
 
   // Reading shorter DNA sequence
   ca::types::Base* sequence_s = new ca::types::Base[s_length];
   for (ca::types::U16 i = 0; i < s_length; i++)
-    sequence_s[i] = (ca::types::Base)fgetc(input_file_header);
-  fgetc(input_file_header);
+    sequence_s[i] = (ca::types::Base)fgetc(input_file_handle);
+  fgetc(input_file_handle);
 
   // Reading longer DNA sequence
   ca::types::Base* sequence_r = new ca::types::Base[r_length];
   for (ca::types::U16 i = 0; i < r_length; i++)
-    sequence_r[i] = (ca::types::Base)fgetc(input_file_header);
+    sequence_r[i] = (ca::types::Base)fgetc(input_file_handle);
 
   // Cleaning up
-  fclose(input_file_header);
+  fclose(input_file_handle);
 
   // Using C's internal clock for perf measuring, fight me
   clock_t start = clock();
