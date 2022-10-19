e6f29b908fcc52acd6e8f0b2aab31f9f100b6cd5
Thiago Ferronatto <thiagoferronatto18@gmail.com>
2022-10-18 21:45:26 -0400

    Sliding window implemented. Latest working build.
    Instead of spreading a grid over an anti-diagonal, we now have a fixed-size grid that slides over it. Much more flexible, although less readable and, most importantly, does not solve the scalability problem we had in the previous version. Results are still correct though. Somebody help me.

--- genetic_distance.cu
+++ genetic_distance.cu
@@ -25,7 +25,7 @@ __device__ inline void CalcCell(const types::Base* sequence_r,
   // Recurrence relation for the DP algorithm is
   // d(i, j) = min{
   //   d(i, j - 1) + 1,
-  //   d(i - 1, j - 1) + {0 if s[j] = r[i], 1 otherwise},
+  //   d(i - 1, j - 1) + {0 if s[i] = r[j], 1 otherwise},
   //   d(i - 1, j) + 1
   // }
   const types::U16 left = dp_table[i * (r_length + 1) + j - 1] + 1;
@@ -41,33 +41,34 @@ __global__ void SolveDPP(const types::Base* sequence_r,
                          const types::Base* sequence_s, types::U16 r_length,
                          types::U16 s_length, types::U16* dp_table) {
   const types::U16 global_id = blockIdx.x * blockDim.x + threadIdx.x;
+  const types::U16 grid_size = blockDim.x * gridDim.x;
 
   // Initializing first row and column
-  if (global_id <= s_length) dp_table[global_id * (r_length + 1)] = global_id;
-  if (global_id <= r_length) dp_table[global_id] = global_id;
+  types::U16 grids_per_row = (r_length + grid_size - 1) / grid_size;
+  for (types::U16 k = 0; k < grids_per_row; k++) {
+    types::U16 curr_id = global_id + k * grid_size;
+    if (curr_id <= r_length) dp_table[curr_id] = curr_id;
+    if (curr_id <= s_length) dp_table[curr_id * (r_length + 1)] = curr_id;
+  }
 
   // Getting access to the whole grid for synchronization purposes
   cooperative_groups::grid_group grid = cooperative_groups::this_grid();
 
   // Goes through every anti-diagonal in the DP table
   for (types::U16 d = 2, i, j; d < s_length + r_length + 1; d++) {
-    // Position of the current thread in anti-diagonal d
-    i = d - global_id;
-    j = global_id;
+    types::U16 grids_per_diag = (s_length + grid_size - 1) / grid_size;
 
-    // If thread is inside the DP table
-    if (0 < i && i <= s_length && 0 < j && j <= r_length)
-      CalcCell(sequence_r, sequence_s, i, j, r_length, dp_table);
+    for (types::U16 k = 0; k < grids_per_diag; k++) {
+      types::U16 curr_id = global_id + k * grid_size;
 
-    // This if statement doesn't really need to be here, since if the grid is
-    // not valid, then there is no sync and therefore the program just won't
-    // work. Still, we left it for completeness' sake.
-    if (grid.is_valid()) {
-      grid.sync();
-    } else {
-      // There really isn't much we can do if somehow this point is reached.
-      printf("Something went terribly wrong\n");
+      i = d - curr_id;
+      j = curr_id;
+
+      if (0 < i && i <= s_length && 0 < j && j <= r_length)
+        CalcCell(sequence_r, sequence_s, i, j, r_length, dp_table);
     }
+
+    grid.sync();
   }
 }
 
@@ -90,11 +91,19 @@ __host__ types::U16* LaunchKernel(const types::Base* h_sequence_r,
   CHECK(cudaMemcpy(d_sequence_s, h_sequence_s, s_length * sizeof(types::Base),
                    cudaMemcpyHostToDevice));
 
-  // TODO: try different values and test for performance
-  constexpr types::U16 kBlockSize = 1024;
-  const types::U16 grid_size = (r_length + kBlockSize) / kBlockSize;
+  constexpr types::U16 block_size = 256;
 
-  printf("block size = %u; grid size = %u\n", kBlockSize, grid_size);
+  // Accessing device properties
+  cudaDeviceProp device_prop;
+  cudaGetDeviceProperties(&device_prop, 0);
+
+  // Getting exact number of blocks that can be in an SM simultaneously
+  int blocks_per_sm;
+  cudaOccupancyMaxActiveBlocksPerMultiprocessor(&blocks_per_sm, SolveDPP,
+                                                block_size, 0);
+
+  // Optimal grid size for the job (hopefully?)
+  const types::U16 grid_size = device_prop.multiProcessorCount * blocks_per_sm;
 
   // Preparing arguments for cooperative kernel launch
   void** args = new void*[5];
@@ -105,9 +114,8 @@ __host__ types::U16* LaunchKernel(const types::Base* h_sequence_r,
   args[4] = &d_dp_table;
 
   // Launching cooperative kernel
-  // FIXME: CUDA Error: too many blocks in cooperative launch
   CHECK(cudaLaunchCooperativeKernel((const void*)SolveDPP, (dim3)grid_size,
-                                    (dim3)kBlockSize, args, (size_t)0,
+                                    (dim3)block_size, args, (size_t)0,
                                     (cudaStream_t)0));
 
   // Transferring output data from device to host
